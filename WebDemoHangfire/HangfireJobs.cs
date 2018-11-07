c4b828b9d41b77efd46f606dcc92bea327881db0
Paulo Henrique Vieira Junior - Externo 7Comm <paulo.vieira@sifra.local>
2018-11-07 11:16:48 -0200

    Projeto hangfire demo

---
+++ WebDemoHangfire/HangfireJobs.cs
@@ -0,0 +1,29 @@
+﻿using Hangfire;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebDemoHangfire
+{
+    public class HangfireJobs
+    {
+        public static void Register()
+        {
+            // Executado apenas uma vez quando chamado. "Fire and Forget"
+            var jobFireForget = BackgroundJob.Enqueue(() => Debug.WriteLine($"Fire and forget: {DateTime.Now}"));
+
+            // Job executado em um momento específico... 
+            var jobDelayedEspecificTime = BackgroundJob.Schedule(() => Debug.WriteLine($"Delayed: {DateTime.Now}"), DateTimeOffset.Now.AddSeconds(2));
+            // ou em quando tempo após criado, que deve ser executado.
+            var jobDelayed = BackgroundJob.Schedule(() => Debug.WriteLine($"Delayed: {DateTime.Now}"), TimeSpan.FromSeconds(30));
+
+            // Executa um job após o termino de outro. Assim você cria a execução em sequ~encia de vários passos para o processamento de algumas informações. 
+            BackgroundJob.ContinueWith(jobDelayed, () => Debug.WriteLine($"Continuation: {DateTime.Now}"));
+
+            // Executa um job recorrentemente a cada x espaço de tempo. 
+            RecurringJob.AddOrUpdate(() => Debug.WriteLine($"Recurring: {DateTime.Now}"), Cron.Minutely);
+        }
+    }
+}
---
+++ WebDemoHangfire/Startup.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace WebDemoHangfire
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(x => x.UseMemoryStorage());
+            services.AddMvc();
+
+            GlobalConfiguration.Configuration.UseMemoryStorage();
+
+            HangfireJobs.Register();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+
+            app.UseMvc();
+        }
+    }
+}
